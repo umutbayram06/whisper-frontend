f9eb8e6051ed78ce98d9375ca9101d4c11667689
Umut Bayram <umutbayram380606@gmail.com>
2024-12-12 22:12:28 +0300

    fatih front end changes

--- src/components/chat/MessageItem.jsx
+++ src/components/chat/MessageItem.jsx
@@ -3,45 +3,98 @@ import React from "react";
 
 function MessageItem({ message, decodedToken }) {
   let finalMessageJSX = null;
+  const isSender = message.sender._id === decodedToken.userID;
+
+  const containerClass = `flex mt-2  justify-content-${
+    isSender ? "end" : "start"
+  }`;
+  const messageClass = `p-3 border-round text-white text-left ${
+    isSender ? "bg-green-600" : "bg-gray-200 text-black"
+  }`;
+
   switch (message.type) {
     case "text":
       finalMessageJSX = (
-        <div
-          className={`flex mt-2 justify-content-${
-            message.sender._id == decodedToken.userID ? "end" : "start"
-          } mt-2`}
-        >
-          <p className="text-xl">
-            {message.sender.username}: {message.content}
-          </p>
+        <div className={containerClass}>
+          <div
+            className={messageClass}
+            style={{ maxWidth: "60%", position: "relative" }}
+          >
+            <p className="mb-1 text-black-alpha-90">
+              <strong>{message.sender.username}</strong>: {message.content}
+            </p>
+            <small
+              style={{
+                position: "absolute",
+                bottom: "5px",
+                right: "10px",
+                fontSize: "0.75rem",
+                color: isSender ? "#d1e7dd" : "#6c757d",
+              }}
+            >
+              {new Date(message.time).toLocaleTimeString()}
+            </small>
+          </div>
         </div>
       );
       break;
     case "image":
       finalMessageJSX = (
-        <div
-          className={`flex justify-content-${
-            message.sender._id == decodedToken.userID ? "end" : "start"
-          } mt-2`}
-        >
-          <p className="text-xl">{message.sender.username}: </p>
-          <Image src={message.content} alt="Image" width="250" preview />
+        <div className={containerClass}>
+          <div
+            className={messageClass}
+            style={{ maxWidth: "60%", position: "relative" }}
+          >
+            <p className="mb-1">
+              <strong className="text-black-alpha-90">
+                {message.sender.username}
+              </strong>
+              :
+            </p>
+            <Image src={message.content} alt="Image" width="250" preview />
+            <small
+              style={{
+                position: "absolute",
+                bottom: "5px",
+                right: "10px",
+                fontSize: "0.75rem",
+                color: isSender ? "#d1e7dd" : "#6c757d",
+              }}
+            >
+              {new Date(message.time).toLocaleTimeString()}
+            </small>
+          </div>
         </div>
       );
       break;
     case "video":
       finalMessageJSX = (
-        <div
-          className={`flex justify-content-${
-            message.sender._id == decodedToken.userID ? "end" : "start"
-          } mt-2`}
-        >
-          <p className="text-xl">{message.sender.username}: </p>
-          <video width="350" height="auto" controls>
-            <source src={message.content} type="video/mp4" />
-          </video>
+        <div className={containerClass}>
+          <div
+            className={messageClass}
+            style={{ maxWidth: "60%", position: "relative" }}
+          >
+            <p className="mb-1 text-black-alpha-90">
+              <strong>{message.sender.username}</strong>:
+            </p>
+            <video width="100%" height="auto" controls>
+              <source src={message.content} type="video/mp4" />
+            </video>
+            <small
+              style={{
+                position: "absolute",
+                bottom: "5px",
+                right: "10px",
+                fontSize: "0.75rem",
+                color: isSender ? "#d1e7dd" : "#6c757d",
+              }}
+            >
+              {new Date(message.time).toLocaleTimeString()}
+            </small>
+          </div>
         </div>
       );
+      break;
     default:
       break;
   }
